0a8a2cb1a691b1c2ca09142645ddeda99adfd76f
hait2810 <checked.kenvip10@gmail.com>
2022-02-21 16:21:44 +0700

    update

---
+++ src/admin/cartdetail.js
@@ -0,0 +1,40 @@
+import { get } from "../api/cart";
+import Header from "../components/header";
+const CartDetail = {
+   async render() {
+        return `
+        ${await Header.render()}
+        ${await Header.render()}
+        <div class="w-[1600px] bg-pink-100">
+        <div class="news w-[1170px] mx-auto">
+          <table class="border-collapse w-[100%]">
+            <tr class="bg-[#04AA6D]">
+              <th class="border-[1px] border-solid border-[#ddd] p-[8px]">ID</th>
+              <th class="border-[1px] border-solid border-[#ddd] p-[8px]">Tên sản phẩm</th>
+              <th class="border-[1px] border-solid border-[#ddd] p-[8px]">Giá</th>
+              <th class="border-[1px] border-solid border-[#ddd] p-[8px]">Ảnh</th>
+              <th class="border-[1px] border-solid border-[#ddd] p-[8px]">Mô tả</th>
+              <th class="border-[1px] border-solid border-[#ddd] p-[8px]">Sửa</th>
+              <th class="border-[1px] border-solid border-[#ddd] p-[8px]">Xoá</th>
+            </tr>
+           
+             
+              <tr class="hover:bg-pink-600">
+              <td class="border-[1px] border-solid border-[#ddd] w-[100px] p-[8px]"></td>
+              <td class="border-[1px] border-solid border-[#ddd] p-[8px] w-[100px]"></td>
+              <td class="border-[1px] border-solid border-[#ddd] w-[100px] p-[8px]">VNĐ</td>
+              <td class="border-[1px] border-solid border-[#ddd] p-[8px] w-[100px]"><img src="" class="w-[100%] h-[50px] mx-auto" alt=""></td>
+              <td class=" text-[12px] border-[1px] border-solid border-[#ddd] p-[8px]"></td>
+              <td class="border-[1px] border-solid border-[#ddd] w-[100px] p-[8px] text-center font-bold"><a href="/admin/news//edit">Sửa</a></td>
+              <td class="border-[1px] border-solid border-[#ddd] w-[100px] p-[8px] text-center font-bold"><button data-id="" class="btn_delete font-bold" type="submit">Xoá</button></td>
+              </tr>
+              
+           
+        
+          </table>
+        </div>
+      </div>
+        `;
+    }
+}
+export default CartDetail;
